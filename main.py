e5707d9090c4be59a63c21a39595e7c29d22e2c4
Natnael Teklu <natnaelteklu200@gmail.com>
2024-05-27 15:32:00 +0300

    add words

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("HangMan")
+
+import random 
+
+
+words = ['elite','event','cats','deep','smooth','ring','clear','drawer']    
+
+number = int(random.random()*len(words))
+
+
+
+def pickword():
+    pickedword = words[number]
+    return pickedword
+def dash():
+    for x in range(0,len(pickword())+1):
+        print("_")
+
+# print("HangMan")
+
+print(dash())
+
+userinput = input("guess a character")
