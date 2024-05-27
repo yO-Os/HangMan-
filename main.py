c3f57ffa71cae753e5ec25ec7462122561d13647
Natnael Teklu <natnaelteklu200@gmail.com>
2024-05-27 16:02:16 +0300

    Pickword

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 
 import random 
 
-
 words = ['elite','event','cats','deep','smooth','ring','clear','drawer']    
 
 number = int(random.random()*len(words))
 
 
 
+count = 0
+
 def pickword():
     pickedword = words[number]
     return pickedword
 def dash():
-    for x in range(0,len(pickword())+1):
-        print("_")
-
-# print("HangMan")
+    guessed = []
+    for x in range(0,len(pickword())):
+        print("_", end=' ')
+        
+        # guessed[x] = ['_']
+        if pickword()[x] == userinput:
+                guessed[x] = userinput
+        else: count+=1
+        return guessed
 
 print(dash())
 
-userinput = input("guess a character")
+
+while count < 5:
+    userinput = input("guess a character")
+    print(dash())
+    
+
